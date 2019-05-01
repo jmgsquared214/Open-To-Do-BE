78c07de2e9c2a22ac65ec6366d0001902baa7fea
Daniel Halterman <danielhalterman@Daniels-MacBook-Pro.local>
2019-05-01 10:58:42 -0400

    trying to fix full_name attribute

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
   has_many :lists
   has_many :items, through: :lists
+
+  def full_name
+    object.full_name
+  end
 end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,12 +1,10 @@
+include ActiveModel::Serialization
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :created_at, :full_name, :email, :bio
 
   # Delegate the practical definition of `full_name` to
   # the User model, where it belongs, rather than
   # (re)defining it here.
-  def full_name
-    object.full_name
-  end
 
   def created_at
     object.created_at.strftime('%B %d, %Y')
