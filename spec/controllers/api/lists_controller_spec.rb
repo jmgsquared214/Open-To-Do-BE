754e8ee2d7f9d86a33237749b36c38d2bd2fb353
Daniel Halterman <danielhalterman@Daniels-MacBook-Pro.local>
2019-05-06 15:08:42 -0400

    Trying to create lists and items in terminal

--- spec/controllers/api/lists_controller_spec.rb
+++ spec/controllers/api/lists_controller_spec.rb
@@ -1,10 +1,10 @@
 require 'rails_helper'
 
-RSpec.describe Api::UsersController, type: :controller do
+RSpec.describe Api::ListsController, type: :controller do
 
-  describe "GET #index" do
+  describe "GET #create" do
     it "returns http success" do
-      get :index
+      get :create
       expect(response).to have_http_status(:success)
     end
   end
